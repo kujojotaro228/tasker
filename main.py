c5969a37b1ff543668d0b1500d88a37655259d9b
kujojotaro228 <64678605+kujojotaro228@users.noreply.github.com>
2023-04-29 22:38:00 +0300

    Program
    Example program

--- main.py
+++ main.py
@@ -1,53 +1,52 @@
-import requests
+import os
+import glob
 import pandas as pd
 from bs4 import BeautifulSoup
 
-def extract_info_from_html(html_text):
+def extract_info_from_html(html_file):
     """
-    Extracts information from HTML page using BeautifulSoup library.
+    Extracts relevant information from an HTML file and returns it in a tuple.
 
     Args:
-        html_text (str): HTML code of the page as a string.
+        html_file (str): Path to the HTML file to extract information from.
 
     Returns:
-        Tuple containing extracted information as strings.
+        Tuple containing the extracted information, in the following order:
+        - Name
+        - Surname
+        - Organization
+        - Title
     """
-    soup = BeautifulSoup(html_text, 'html.parser')
-    # Here you can write code to extract the required information from the HTML page
-    # For example, using BeautifulSoup methods, you can find and extract the necessary tags and their contents
-    
-    # Example:
-    name = soup.find('span', {'class': 'name'}).text.strip()
-    surname = soup.find('span', {'class': 'surname'}).text.strip()
-    organization = soup.find('span', {'class': 'organization'}).text.strip()
-    title = soup.find('h1', {'class': 'title'}).text.strip()
+    with open(html_file, 'r') as file:
+        soup = BeautifulSoup(file, 'html.parser')
+        # Here, you can use BeautifulSoup's methods to extract the relevant information from the HTML file.
+        # For example:
+        name = soup.find('span', {'class': 'name'}).text.strip()
+        surname = soup.find('span', {'class': 'surname'}).text.strip()
+        organization = soup.find('span', {'class': 'organization'}).text.strip()
+        title = soup.find('h1', {'class': 'title'}).text.strip()
 
-    # Here you can add additional processing if some data is missing
+        # Here, you can add additional processing if some data is missing.
 
-    return name, surname, organization, title
+        return name, surname, organization, title
 
-
-def process_html_urls(urls):
+def process_html_files(folder_path):
     """
-    Processes list of URLs pointing to HTML pages, extracts required information from them
-    and saves the data to an Excel file.
+    Processes all HTML files in a folder, extracts relevant information from them,
+    and saves the information in an Excel file.
 
     Args:
-        urls (list of str): List of URLs pointing to HTML pages.
+        folder_path (str): Path to the folder containing the HTML files.
     """
+    html_files = glob.glob(os.path.join(folder_path, '*.html'))
     data = []
     
-    for url in urls:
-        response = requests.get(url)
-        if response.status_code != 200:
-            print(f"Error: {response.status_code} - {url}")
-            continue
-        
-        name, surname, organization, title = extract_info_from_html(response.text)
-        # Here you can request missing information if necessary
+    for html_file in html_files:
+        name, surname, organization, title = extract_info_from_html(html_file)
+        # Here, you can request missing information if necessary.
         # For example:
         if not name:
-            name = input(f"Enter name for URL {url}: ")
+            name = input("Enter name for file {}: ".format(html_file))
 
         data.append({
             'Name': name,
@@ -60,12 +59,6 @@ def process_html_urls(urls):
     df.to_excel('output.xlsx', index=False)
     print("List successfully created and saved to 'output.xlsx' file.")
 
-# List of URLs pointing to HTML pages
-urls = [
-    'https://example.com/page1.html',
-    'https://example.com/page2.html',
-    'https://example.com/page3.html',
-]
-
-# Process the HTML URLs and save the data to Excel file
-process_html_urls(urls)
+# Example usage:
+folder_path = 'path/to/folder'
+process_html_files(folder_path)
