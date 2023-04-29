c96fd73e265ef1fceb9d0b9fb2ddd444ddd1c554
kujojotaro228 <64678605+kujojotaro228@users.noreply.github.com>
2023-04-29 22:30:35 +0300

    Program
    This program is a Python script that extracts information from HTML files or web pages and saves it to an Excel file. It uses the BeautifulSoup library to parse HTML files and Pandas library to create and manipulate data frames. The program first searches for all HTML files in a specified directory, then extracts the desired information from each file, such as name, surname, organization, and title. If any information is missing, it prompts the user to input it. Finally, it saves the extracted data as an Excel file named "output.xlsx".

--- main.py
+++ main.py
@@ -1,47 +1,71 @@
-import pandas as pd
 import requests
+import pandas as pd
 from bs4 import BeautifulSoup
 
+def extract_info_from_html(html_text):
+    """
+    Extracts information from HTML page using BeautifulSoup library.
 
-def extract_info_from_html(url):
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
-    # Здесь вам нужно написать код для извлечения нужной информации из HTML-страницы
-    # Например, используя методы BeautifulSoup, вы можете найти и извлечь нужные теги и их содержимое
+    Args:
+        html_text (str): HTML code of the page as a string.
 
-    # Пример:
+    Returns:
+        Tuple containing extracted information as strings.
+    """
+    soup = BeautifulSoup(html_text, 'html.parser')
+    # Here you can write code to extract the required information from the HTML page
+    # For example, using BeautifulSoup methods, you can find and extract the necessary tags and their contents
+    
+    # Example:
     name = soup.find('span', {'class': 'name'}).text.strip()
     surname = soup.find('span', {'class': 'surname'}).text.strip()
     organization = soup.find('span', {'class': 'organization'}).text.strip()
     title = soup.find('h1', {'class': 'title'}).text.strip()
 
-    # Здесь вы можете добавить дополнительную обработку, если какие-то данные отсутствуют
+    # Here you can add additional processing if some data is missing
 
     return name, surname, organization, title
 
 
-def process_html_files(urls):
-    data = []
+def process_html_urls(urls):
+    """
+    Processes list of URLs pointing to HTML pages, extracts required information from them
+    and saves the data to an Excel file.
 
+    Args:
+        urls (list of str): List of URLs pointing to HTML pages.
+    """
+    data = []
+    
     for url in urls:
-        name, surname, organization, title = extract_info_from_html(url)
-        # Здесь можно запросить недостающую информацию, если требуется
-        # Например:
+        response = requests.get(url)
+        if response.status_code != 200:
+            print(f"Error: {response.status_code} - {url}")
+            continue
+        
+        name, surname, organization, title = extract_info_from_html(response.text)
+        # Here you can request missing information if necessary
+        # For example:
         if not name:
-            name = input("Введите имя для URL-адреса {}: ".format(url))
+            name = input(f"Enter name for URL {url}: ")
 
         data.append({
-            'Имя': name,
-            'Фамилия': surname,
-            'Организация': organization,
-            'Название': title
+            'Name': name,
+            'Surname': surname,
+            'Organization': organization,
+            'Title': title
         })
-
+    
     df = pd.DataFrame(data)
     df.to_excel('output.xlsx', index=False)
-    print("Список успешно создан и сохранен в файл 'output.xlsx'.")
+    print("List successfully created and saved to 'output.xlsx' file.")
 
+# List of URLs pointing to HTML pages
+urls = [
+    'https://example.com/page1.html',
+    'https://example.com/page2.html',
+    'https://example.com/page3.html',
+]
 
-# Список URL-адресов для обработки
-urls = ['https://www.example.com/page1.html', 'https://www.example.com/page2.html']
-process_html_files(urls)
+# Process the HTML URLs and save the data to Excel file
+process_html_urls(urls)
